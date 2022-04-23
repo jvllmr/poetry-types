50888147407d2d3f61113418b48e1a9893ea871e
kreyoo <zunder325@gmail.com>
2022-04-23 19:06:56 +0200

    fix poetry returning return code 113

--- poetry_types/poetry_types.py
+++ poetry_types/poetry_types.py
@@ -1,3 +1,5 @@
+
+
 from poetry.console.commands.remove import RemoveCommand
 from poetry.plugins import ApplicationPlugin
 from poetry.console.application import Application
@@ -5,8 +7,9 @@ from poetry.console.application import Application
 from poetry.console.commands.update import UpdateCommand
 from poetry.console.commands.add import AddCommand
 
-from cleo.events.console_events import COMMAND
+from cleo.events.console_events import TERMINATE
 from cleo.events.console_command_event import ConsoleCommandEvent
+from cleo.io.io import IO
 from poetry_types.commands import (
     AddTypesCommand,
     RemoveTypesCommand,
@@ -16,18 +19,16 @@ from poetry_types.commands import (
 
 class PoetryTypes(ApplicationPlugin):
     def activate(self, app: Application):
-        app.event_dispatcher.add_listener(COMMAND, self.onCommand)
+        app.event_dispatcher.add_listener(TERMINATE, self.onCommand)
         app.command_loader.register_factory("types add", AddTypesCommand.factory)
         app.command_loader.register_factory("types remove", RemoveTypesCommand.factory)
         app.command_loader.register_factory("types update", UpdateTypesCommand.factory)
 
     def onCommand(self, event: ConsoleCommandEvent, *_):
         command = event.command
+        
         io = event.io
-
-        if command.run(io):
-            return
-        event.disable_command()
+        
         io.write_line("")
 
         if isinstance(command, UpdateCommand):
@@ -36,3 +37,6 @@ class PoetryTypes(ApplicationPlugin):
             AddTypesCommand().run_after(io, command)
         elif isinstance(command, RemoveCommand):
             RemoveTypesCommand().run_after(io, command)
+
+
+       
